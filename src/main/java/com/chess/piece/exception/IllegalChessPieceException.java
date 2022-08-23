7127172829ac2e0ea8b1bd99895e3e6eea74efd7
Govind <govindkedia71086@gmail.com>
2022-08-23 20:28:49 +0530

    Adding Java docs

--- src/main/java/com/chess/piece/exception/IllegalChessPieceException.java
+++ src/main/java/com/chess/piece/exception/IllegalChessPieceException.java
@@ -1,5 +1,8 @@
 package com.chess.piece.exception;
 
+/**
+ * Exception class chess piece.
+ */
 public class IllegalChessPieceException extends  Exception{
     public IllegalChessPieceException(String message){
         super(message);
